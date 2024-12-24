7a119eb4d11607cacfe339625bf1e5020701dffc
lightfrantice <lightfrantice@gmail.com>
2024-12-24 23:55:52 +0900

    add partial logic test of add method

--- test.py
+++ test.py
@@ -1,10 +1,15 @@
-from baseModels import DataModel
+from dataModel import DataModel
 import formatCollection as fc
 
-dataBinStr = '0011010'
+dataBinStr0 = '0011010'
+dataBinStr1 = '0011010'
 
 fm = fc.freeFloat(3, 3)
 
-data = DataModel(dataBinStr, fm)
+data0 = DataModel(dataBinStr0, fm)
+data1 = DataModel(dataBinStr1, fm)
 
-print(data.getValue())
+print(data0.getValue())
+print(data1.getValue())
+
+print((data0+data1).getValue())
